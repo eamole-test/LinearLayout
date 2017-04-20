e239e64d89354e549156080a02fb9c8f7838c239
Eamonn <eamonn.ocochlain@gmail.com>
2017-04-20 10:12:10 +0100

    Initial commit

---
+++ app/src/main/java/ie/corktrainingcentre/linearlayout/LinearLayoutMain.java
@@ -0,0 +1,18 @@
+package ie.corktrainingcentre.linearlayout;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class LinearLayoutMain extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rainbow);
+    }
+
+
+}
